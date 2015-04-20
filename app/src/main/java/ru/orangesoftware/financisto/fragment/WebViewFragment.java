6f8882350869c0fe732e51126d50d250a0f2dd46
NienKun Yeh <ynk0917@gmail.com>
2015-04-20 11:47:48 +0800

    add custom WebViewFragment

---
+++ app/src/main/java/ru/orangesoftware/financisto/fragment/WebViewFragment.java
@@ -0,0 +1,23 @@
+package ru.orangesoftware.financisto.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class WebViewFragment extends android.webkit.WebViewFragment {
+    private static final String EXTRA_URL = "url";
+
+    public static Fragment newInstance(String url) {
+        Fragment fragment = new WebViewFragment();
+        Bundle args = new Bundle();
+        args.putString(EXTRA_URL, url);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String url = getArguments().getString(EXTRA_URL);
+        getWebView().loadUrl(url);
+    }
+}
